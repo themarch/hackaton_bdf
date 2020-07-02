cc088d75e5a313f5ac75cf9d8e7d48554996788d
themarch <theomarchandarvierlyon@gmail.com>
2020-07-02 13:57:42 +0200

    sucess

--- database/migrations/2020_06_24_083522_create_article_table.php
+++ database/migrations/2020_06_24_083522_create_article_table.php
@@ -15,14 +15,14 @@ class CreateArticleTable extends Migration
     {
         Schema::create('article', function (Blueprint $table) {
             $table->increments('uniqid');
-            $table->string('link_paper', '2048')->nullable();
-            $table->string('name_paper', '2048')->nullable();
+            $table->string('link_paper', '1048')->nullable();
+            $table->string('name_paper', '8096')->nullable();
             $table->string('id_auteur', '255')->nullable();
             $table->string('JEL_name', '2048')->nullable();
-            $table->string('JEL_1', '1024')->nullable();
-            $table->string('JEL_2', '2048')->nullable();
-            $table->string('JEL_3', '2048')->nullable();
-            $table->string('JEL_4', '2048')->nullable();
+            $table->string('JEL_1', '255')->nullable();
+            $table->string('JEL_2', '1024')->nullable();
+            $table->string('JEL_3', '1024')->nullable();
+            $table->string('JEL_4', '1024')->nullable();
             $table->timestamps();
         });
     }
